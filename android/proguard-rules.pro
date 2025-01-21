c6d9edcfc08abf8914f722b6cf349b4214277c6e
M-Ahal <m.ahal@dimerce.com>
2025-01-21 11:39:05 +0100

    fix cannot find BarcodeReader class plugin registrant error
    -> progress?

---
+++ android/proguard-rules.pro
@@ -0,0 +1,1 @@
+-keep class com.honeywell.aidc.** { *; }
--- android/src/main/java/com/wyb/flutter_warehouse_pda_scanner/FlutterWarehousePdaScannerPlugin.java
+++ android/src/main/java/com/wyb/flutter_warehouse_pda_scanner/FlutterWarehousePdaScannerPlugin.java
@@ -5,8 +5,21 @@ import android.content.BroadcastReceiver;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Build;
+import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+
+import com.honeywell.aidc.AidcManager;
+import com.honeywell.aidc.BarcodeFailureEvent;
+import com.honeywell.aidc.BarcodeReadEvent;
+import com.honeywell.aidc.BarcodeReader;
+import com.honeywell.aidc.InvalidScannerNameException;
+import com.honeywell.aidc.UnsupportedPropertyException;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import io.flutter.embedding.engine.plugins.FlutterPlugin;
 import io.flutter.plugin.common.EventChannel;
@@ -18,7 +31,7 @@ import io.flutter.plugin.common.MethodChannel.Result;
 /**
  * FlutterWarehousePdaScannerPlugin
  */
-public class FlutterWarehousePdaScannerPlugin implements FlutterPlugin, MethodCallHandler {
+public class FlutterWarehousePdaScannerPlugin implements FlutterPlugin, MethodCallHandler, BarcodeReader.BarcodeListener {
     /// The MethodChannel that will the communication between Flutter and native Android
     ///
     /// This local reference serves to register the plugin with the Flutter Engine and unregister it
@@ -64,7 +77,7 @@ public class FlutterWarehousePdaScannerPlugin implements FlutterPlugin, MethodCa
         );
 
         applicationContext = flutterPluginBinding.getApplicationContext();
-
+        initHoneywell();
     }
 
     @Override
@@ -82,6 +95,8 @@ public class FlutterWarehousePdaScannerPlugin implements FlutterPlugin, MethodCa
     }
 
     private BroadcastReceiver createReceiver(final EventChannel.EventSink events) {
+
+
         return new BroadcastReceiver() {
             @Override
             public void onReceive(Context context, Intent intent) {
@@ -98,5 +113,72 @@ public class FlutterWarehousePdaScannerPlugin implements FlutterPlugin, MethodCa
                 events.success(intentData);
             }
         };
+    }
+
+    private com.honeywell.aidc.BarcodeReader barcodeReader;
+    private ListView barcodeList;
+    private AidcManager manager;
+
+    private void initHoneywell() {
+        // create the AidcManager providing a Context and a
+        // CreatedCallback implementation.
+        AidcManager.create(applicationContext, aidcManager -> {
+            manager = aidcManager;
+            try{
+                barcodeReader = manager.createBarcodeReader();
+            }
+            catch (InvalidScannerNameException e){
+                System.out.println("Invalid Scanner Name Exception: " + e.getMessage());
+            }
+            catch (Exception e){
+                System.out.println("Exception: " + e.getMessage());
+            }
+        });
+
+        if (barcodeReader == null) {
+            System.out.println("Error: No barcode reader created");
+            return;
+        }
+        barcodeReader.addBarcodeListener(this);
+        // set the trigger mode to client control
+        try {
+            barcodeReader.setProperty(BarcodeReader.PROPERTY_TRIGGER_CONTROL_MODE,
+                    BarcodeReader.TRIGGER_CONTROL_MODE_AUTO_CONTROL);
+        } catch (UnsupportedPropertyException e) {
+            System.out.println("Failed to apply properties");
+        }
+        Map<String, Object> properties = new HashMap<>();
+        // Set Symbologies On/Off
+        properties.put(BarcodeReader.PROPERTY_CODE_128_ENABLED, true);
+        properties.put(BarcodeReader.PROPERTY_GS1_128_ENABLED, true);
+        properties.put(BarcodeReader.PROPERTY_QR_CODE_ENABLED, true);
+        properties.put(BarcodeReader.PROPERTY_CODE_39_ENABLED, true);
+        properties.put(BarcodeReader.PROPERTY_DATAMATRIX_ENABLED, true);
+        properties.put(BarcodeReader.PROPERTY_UPC_A_ENABLE, true);
+        properties.put(BarcodeReader.PROPERTY_EAN_13_ENABLED, false);
+        properties.put(BarcodeReader.PROPERTY_AZTEC_ENABLED, false);
+        properties.put(BarcodeReader.PROPERTY_CODABAR_ENABLED, false);
+        properties.put(BarcodeReader.PROPERTY_INTERLEAVED_25_ENABLED, false);
+        properties.put(BarcodeReader.PROPERTY_PDF_417_ENABLED, false);
+        // Set Max Code 39 barcode length
+        properties.put(BarcodeReader.PROPERTY_CODE_39_MAXIMUM_LENGTH, 10);
+        // Turn on center decoding
+        properties.put(BarcodeReader.PROPERTY_CENTER_DECODE, true);
+        // Enable bad read response
+        properties.put(BarcodeReader.PROPERTY_NOTIFICATION_BAD_READ_ENABLED, true);
+        // Sets time period for decoder timeout in any mode
+        properties.put(BarcodeReader.PROPERTY_DECODER_TIMEOUT,  400);
+        // Apply the settings
+        barcodeReader.setProperties(properties);
+    }
+
+    @Override
+    public void onBarcodeEvent(BarcodeReadEvent barcodeReadEvent) {
+        System.out.println("Barcode read event happened -> " + '"' + barcodeReadEvent.getBarcodeData() + '"');
+    }
+
+    @Override
+    public void onFailureEvent(BarcodeFailureEvent barcodeFailureEvent) {
+        System.out.println("Barcode failure event happened -> " + '"' + barcodeFailureEvent.toString() + '"');
     }
 }
